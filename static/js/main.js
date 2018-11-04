088a244bd1ef647edbff7df0eed6d73a8df18d7f
Rishi Maheshwari <rishim@berkeley.edu>
2018-11-03 22:43:54 -0700

    ajax for pdfs

--- static/js/main.js
+++ static/js/main.js
@@ -70,6 +70,22 @@ $(document).ready(function(){
             contentType: false,
             processData: false,
 
+            xhr: function() {
+                var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) {
+                    // For handling the progress of the upload
+                    myXhr.upload.addEventListener('progress', function(e) {
+                        if (e.lengthComputable) {
+                            $('progress').attr({
+                                value: e.loaded,
+                                max: e.total,
+                            });
+                        }
+                    } , false);
+                }
+                return myXhr;
+            }
+            
             success: function(response) {
                 console.log("success!");
                 $('#response_pdf').text(response);
