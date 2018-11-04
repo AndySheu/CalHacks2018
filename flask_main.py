4cb375b7e82470d9d9eab3db0a86c69654639771
Rishi Maheshwari <rishim@berkeley.edu>
2018-11-03 22:16:24 -0700

    changes

--- flask_main.py
+++ flask_main.py
@@ -7,18 +7,15 @@ def search():
     term = request.form['input_text']
     return summarize.main(term)
 
-@app.route('/summarizePDF', methods = ['Post'])
-def summarize():
-	pdf_file = request.form['pdf_upload']
-	file_type = request.form['file_type']
-	return summarize.pdf(pdf_file)
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/file', methods=['Post'])
-def save(fi, ty):
+@app.route('/summarizePDF', methods = ['Post'])
+def save():
+	fi = request.form['pdf_upload']
+	ty = request.form['file_type']
+
     file = open('a.' + ty, 'w')
     file.write(fi)
     file.close()
