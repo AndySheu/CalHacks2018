b1fe3defbf4160fb97c212e2c390c9c279f245d0
Andy Sheu <andysheu@berkeley.edu>
2018-11-03 19:30:41 -0700

    update

--- flask_main.py
+++ flask_main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/summarizeText', methods=['Post'])
 def search():
-	term = request.form['input_text']
-	return summarize.summarize(term)
+    term = request.form['input_text']
+    return summarize.summarize(term)
 
 @app.route('/')
 def index():
